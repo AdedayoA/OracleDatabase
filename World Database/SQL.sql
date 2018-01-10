1ae5bbcd921ec18f68eac2349352cf6b03f7f34b
Desktop_BOT <daworunse@gmail.com>
2018-01-10 16:24:41 -0600

    Selecting Rows

--- World Database/SQL.sql
+++ World Database/SQL.sql
@@ -14,3 +14,18 @@ SELECT Name AS "Name Of Country", LifeExpectancy AS "Life Expectancy" FROM Count
 
 --Retrives the number of rows from the country table
 SELECT COUNT(*) FROM Country;
+
+/\ SELECTing Rows /\
+
+--Retrieves the name, contient, and region from the country table
+SELECT Name, Continent, Region FROM Country;
+
+--Retrieves the name, contient, and region from the country table, only where the continent is equal is Europe
+SELECT Name, Continent, Region FROM Country WHERE Continent = 'Europe';
+
+--Retrieves the first 5 rows with the name, contient, and region from the country table, only where the continent is equal is Europe
+SELECT *
+FROM
+( SELECT Name, Continent, Region FROM Country WHERE Continent = 'Europe') 
+where ROWNUM <= 5;
+
