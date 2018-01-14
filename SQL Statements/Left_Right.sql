5b6dd3b8d8ea6711a84345050a3edda29c321ab7
Desktop_BOT <daworunse@gmail.com>
2018-01-13 19:55:10 -0600

    New table added

---
+++ SQL Statements/Left_Right.sql
@@ -0,0 +1,22 @@
+CREATE TABLE left ( id NUMBER, description VARCHAR2(30));
+CREATE TABLE right ( id NUMBER, description VARCHAR2(30));
+
+INSERT INTO left VALUES ( 1, 'left 01' );
+INSERT INTO left VALUES ( 2, 'left 02' );
+INSERT INTO left VALUES ( 3, 'left 03' );
+INSERT INTO left VALUES ( 4, 'left 04' );
+INSERT INTO left VALUES ( 5, 'left 05' );
+INSERT INTO left VALUES ( 6, 'left 06' );
+INSERT INTO left VALUES ( 7, 'left 07' );
+INSERT INTO left VALUES ( 8, 'left 08' );
+INSERT INTO left VALUES ( 9, 'left 09' );
+
+INSERT INTO right VALUES ( 6, 'right 06' );
+INSERT INTO right VALUES ( 7, 'right 07' );
+INSERT INTO right VALUES ( 8, 'right 08' );
+INSERT INTO right VALUES ( 9, 'right 09' );
+INSERT INTO right VALUES ( 10, 'right 10' );
+INSERT INTO right VALUES ( 11, 'right 11' );
+INSERT INTO right VALUES ( 11, 'right 12' );
+INSERT INTO right VALUES ( 11, 'right 13' );
+INSERT INTO right VALUES ( 11, 'right 14' );
